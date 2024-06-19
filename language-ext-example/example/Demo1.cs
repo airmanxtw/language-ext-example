2c656bc1550b6951158593fb93dab0df9977c7c9
airmanxtw <airmanx@stust.edu.tw>
2024-06-19 09:54:44 +0800

    demo

---
+++ language-ext-example/example/Demo1.cs
@@ -0,0 +1,43 @@
+namespace language_ext_example.example;
+
+using System.Globalization;
+using LanguageExt;
+using LanguageExt.ClassInstances;
+using LanguageExt.Common;
+using LanguageExt.Pretty;
+using static LanguageExt.Prelude;
+
+public class Demo1
+{
+
+    public static Option<float> Pi() => 3.14f;
+
+    // 求平方
+    public static Option<float> Square(float r) => Some(r * r);
+
+    public static float Square2(float r) => r * r;
+
+    public static Option<float> Square3(float r) =>
+    Try(() => Square2(r))
+    .Match(
+        Succ: v => Some(v),
+        Fail: e => None
+    );
+
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+    // 求面積
+    public static Option<float> Area(float r) => Pi().Map(x => Square(r)).Flatten();
+
+
+
+    public static Option<float> Area2(float r) => Pi().Bind(x => Square(r));
+
+    public static Option<float> Area3(float r) => Pi().Map(x => Square2(r));
+
+
+ 
+
+}
---
+++ test-language-ext-example/Test/Demo1Test.cs
@@ -0,0 +1,17 @@
+namespace test_language_ext_example.Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using language_ext_example.example;
+using LanguageExt;
+using LanguageExt.UnitTesting;
+using LanguageExt.ClassInstances;
+using LanguageExt.UnsafeValueAccess;
+
+[TestClass]
+public class Demo1Test
+{
+    [TestMethod]
+    public void Test()
+    {
+        Assert.AreEqual(1, 1);
+    }
+}
