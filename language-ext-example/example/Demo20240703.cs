bda15c39161f1c52b11e4d35f072ce7a0b2c842e
airmanxtw <airmanx@stust.edu.tw>
2024-07-03 08:22:23 +0800

    add class

---
+++ language-ext-example/example/Demo20240703.cs
@@ -0,0 +1,13 @@
+namespace language_ext_example.example;
+
+using System.Globalization;
+using LanguageExt;
+using LanguageExt.ClassInstances;
+using LanguageExt.Common;
+using LanguageExt.Pretty;
+using static LanguageExt.Prelude;
+
+public class Demo20240703
+{
+
+}
