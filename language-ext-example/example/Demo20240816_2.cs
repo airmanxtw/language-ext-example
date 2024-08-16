a85bdbc50e3810837b4044cea09abf3cae6e04a3
airmanxtw <airmanx@stust.edu.tw>
2024-08-16 10:00:28 +0800

    feat: Refactor Demo20240816_2.cs for improved readability and maintainability

--- language-ext-example/example/Demo20240816_2.cs
+++ language-ext-example/example/Demo20240816_2.cs
@@ -7,16 +7,28 @@ using LanguageExt.ClassInstances;
 using LanguageExt.Common;
 using LanguageExt.Pretty;
 using LanguageExt.SomeHelp;
+using LanguageExt.UnsafeValueAccess;
 using static LanguageExt.Prelude;
 public class Demo20240816_2
 {
-    public Func<byte[],byte[],int> TotalBytes = (byte[] a, byte[] b) => a.Length + b.Length;
+    public Func<byte[], byte[], int> TotalBytes = (byte[] a, byte[] b) => a.Length + b.Length;
 
-    public Eff<byte[]> GetFile(string path)=>
-    Eff(()=>File.ReadAllBytes(path))
-    .MapFail(ex=>$"讀取檔案{path}發生錯誤:{ex.Message}");
+    public Eff<byte[]> GetFile(string path) =>
+    Eff(() => File.ReadAllBytes(path))
+    .MapFail(ex => $"讀取檔案{path}發生錯誤:{ex.Message}");
 
-    public Fin<int> GetFileTotalBytes(string path1,string path2)=>
-    TotalBytes.Apply(GetFile(path1).Run(),GetFile(path2).Run());
+    public Fin<int> GetFileTotalBytes(string path1, string path2) =>
+    TotalBytes.Apply(GetFile(path1).Run(), GetFile(path2).Run());
+
+    public Eff<int> GetAllFileTotalBytes1(string[] paths) =>
+    paths.Map(GetFile).Sequence().Map(v => v.Count());
+
+    public int GetAllFileTotalBytes2(string[] paths) =>
+    paths.Map(GetFile)
+    .SequenceParallel<Eff<byte[]>, int>((b) => b.Run().Match(Succ: c => c.Length, Fail: _ => 0))
+    .Map(v => v.Sum())
+    .ValueUnsafe()
+    .Result;
+
 
 }
