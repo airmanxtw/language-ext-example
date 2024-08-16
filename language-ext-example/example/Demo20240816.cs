0c7a77e5b1b433632f542781fb8a8b40596ad616
airmanxtw <airmanx@stust.edu.tw>
2024-08-16 09:12:49 +0800

    feat: Refactor Demo20240816.cs for improved readability and maintainability

--- language-ext-example/example/Demo20240816.cs
+++ language-ext-example/example/Demo20240816.cs
@@ -11,19 +11,19 @@ using LanguageExt.SomeHelp;
 using static LanguageExt.Prelude;
 public class Demo20240816
 {
-    public static int Add(int a, int b) => a + b;
+    public  Func<int,int,int>  Add = (int a, int b) => a + b;
 
     public static Option<int> GetA(int a) => Some(a);
 
     public static Option<int> GetB(int b) => Some(b);
 
-    public static Option<int> GetResult1(int a, int b) =>
+    public  Option<int> GetResult1(int a, int b) =>
     GetA(a).Bind(a1 => GetB(b).Bind(b1 => Some(Add(a1, b1))));
 
-    public static Option<int> GetResult2(int a, int b) =>
-    fun((int a, int b) => Add(a, b)).Apply(GetA(a), GetB(b));
+    public  Option<int> GetResult2(int a, int b) =>
+    Add.Apply(GetA(a), GetB(b));
 
-    public static Option<int> GetResult3(int a, int b) =>
+    public  Option<int> GetResult3(int a, int b) =>
     from a1 in GetA(a)
     from b1 in GetB(b)
     select Add(a1, b1);
