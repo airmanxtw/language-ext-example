e5e9583d2c5c4e11ae85d201e237b8f5c90a1d63
airmanxtw <airmanx@stust.edu.tw>
2024-09-12 00:40:05 +0800

    Refactor Validation.cs for improved readability and maintainability

--- language-ext-example/example/Validation.cs
+++ language-ext-example/example/Validation.cs
@@ -1,5 +1,6 @@
 using LanguageExt;
 using LanguageExt.Common;
+using LanguageExt.UnsafeValueAccess;
 using static LanguageExt.Prelude;
 
 namespace language_ext_example.example;
@@ -10,7 +11,7 @@ public class ValidationExm
         ('A', 10), ('B', 11), ('C', 12), ('D', 13), ('E', 14), ('F', 15), ('G', 16), ('H', 17), ('J', 18),
         ('K', 19), ('L', 20), ('M', 21), ('N', 22), ('P', 23), ('Q', 24), ('R', 25), ('S', 26), ('T', 27),
         ('U', 28), ('V', 29), ('X', 30), ('Y', 31), ('W', 32), ('Q', 33), ('I', 34), ('O', 35)
-    ).Find(c).IfNone(0);
+    ).Find(c).IfNone(-1);
 
     public static Validation<Error, char> IsNum(char c) =>
     c >= 48 && c <= 57 ? Success<Error, char>(c) : Fail<Error, char>(Error.New($"這不是數字(0~9):{c}"));
@@ -20,6 +21,18 @@ public class ValidationExm
 
     public static Validation<Error, string> IsTen(string s) =>
     pipe(s, v => v.Length, Cond<int>(v => v == 10).Then(Success<Error, string>(s)).Else(Fail<Error, string>(Error.New("長度不是10"))));
+
+    public static IEnumerable<char> GetUniCode(IEnumerable<char> id) =>
+    [.. id.Fold(Lst<char>.Empty, (acc, c) => GetCode(c) != -1 ? acc.AddRange(GetCode(c).ToString().ToCharArray()) : acc.Add(c))];
+
+    public static IEnumerable<char> GetSpecCode() => "1987654321".ToCharArray();
+
+    public static IEnumerable<int> MutilpUniAndSpec(IEnumerable<char> id) =>
+    GetUniCode(id).Map((i, c) => parseInt(c.ToString()).Value() * parseInt(GetSpecCode().Skip(i).Take(1).First().ToString()).Value())
+    .Map(c => parseInt(c.ToString().Last().ToString()).Value());
+
+    public static int GetCheckCode(IEnumerable<char> id) => 10 - parseInt(MutilpUniAndSpec(id).Sum().ToString().Last().ToString()).Value();
+
 
     public static Either<Seq<Error>, IEnumerable<char>> CheckID(string s) =>
     (from _ in guard(s.Length == 10, Error.New("長度不是10")).ToValidation()
@@ -31,13 +44,12 @@ public class ValidationExm
     //var result=List(IsLetter(s.First()),IsNum(s.Skip(1).Take(1).First()));
 }
 
-    // public static Either<Error, string> IsID(string s)
-    // {
-    //     var result = guard(s.Length == 10, Error.New("長度不是10")).ToEither()
-    //                 .Map(_ => IsLetter(s.First()));
+// public static Either<Error, string> IsID(string s)
+// {
+//     var result = guard(s.Length == 10, Error.New("長度不是10")).ToEither()
+//                 .Map(_ => IsLetter(s.First()));
 
-    // }
+// }
 
 
 
-}
