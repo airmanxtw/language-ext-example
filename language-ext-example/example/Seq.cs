dc0ff6129cf44a3500eccc1a452b9ea2860ae876
airmanxtw <airmanx@stust.edu.tw>
2024-10-12 08:43:27 +0800

    Refactor Seq.cs for improved readability and maintainability

--- language-ext-example/example/Seq.cs
+++ language-ext-example/example/Seq.cs
@@ -6,9 +6,12 @@ using static LanguageExt.Prelude;
 
 public class SeqExm
 {
-    private static Either<string, int> IsEven(int i) => i % 2 == 0 ? Right<string, int>(i) : Left<string, int>("Not even");
-    public static Either<string, int> GetFirstOdd1() => List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).Map(IsEven).Where(x => x.IsRight).FirstOrDefault();
+    private static Either<string, int> IsEven(int i) =>
+    i % 2 == 0 ? Right<string, int>(i) : Left<string, int>("Not even");
+    public static Either<string, int> GetFirstOdd1() =>
+    List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).Map(IsEven).Where(x => x.IsRight).FirstOrDefault();
 
     // faster than GetFirstOdd1
-    public static Either<string, int> GetFirstOdd2() => Seq(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).Map(IsEven).Where(x => x.IsRight).FirstOrDefault();
+    public static Either<string, int> GetFirstOdd2() =>
+    Seq(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).Map(IsEven).Where(x => x.IsRight).FirstOrDefault();
 }
