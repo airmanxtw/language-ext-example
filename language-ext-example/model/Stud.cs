08631dd2102f8f98b00f54e348b2219cba590ac3
airmanxtw <airmanx@stust.edu.tw>
2024-12-05 00:34:39 +0800

    新增 Chief 類別，調整 Teacher 類別以使用工廠方法創建實例，並更新測試案例以驗證新功能

--- language-ext-example/model/Stud.cs
+++ language-ext-example/model/Stud.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.Contracts;
+
 namespace language_ext_example.model
 {
     public class Stud
@@ -6,5 +8,22 @@ namespace language_ext_example.model
         public int Age { get; set; }
     }
 
-    public record Teacher(string Name, int Age);
+    // 主管
+    public record Chief
+    {
+        public string Name { get; init; } = default!;
+        public int Age { get; init; }
+        public Chief(string name, int age) => (Name, Age) = (name, age);
+    }
+
+    public record Teacher
+    {
+        public required string Name { get; init; }
+
+        public int Age { get; init; }
+
+        [Pure]
+        public static Teacher New(string name, int age) => new() { Name = name, Age = age };
+
+    }
 }
--- test-language-ext-example/Test/LensTest.cs
+++ test-language-ext-example/Test/LensTest.cs
@@ -6,15 +6,36 @@ using LanguageExt.UnitTesting;
 using LanguageExt.ClassInstances;
 using LanguageExt.UnsafeValueAccess;
 using language_ext_example.model;
+using static LanguageExt.Prelude;
 
 [TestClass]
 
 public class LensTest
 {
+
+    public void Test0()
+    {
+        var chief = new Chief("John", 30);
+
+        var teacher = Teacher.New("John", 30);
+
+        var TeacherTom = teacher with { Name = "Tom" };
+
+
+
+        var result = pipe(Teacher.New("John", 30), t => t.Name);
+        Assert.AreEqual("John", result);
+    }
+
+    public void Test1()
+    {
+
+    }
+
     [TestMethod]
     public void TestGetLens()
     {
-        var teacher = new Teacher("John", 30);
+        var teacher = Teacher.New("John", 30);
         var lens = new language_ext_example.example.Lens();
         var result = lens.TeacherNameLens.Get(teacher);
         Assert.AreEqual("John", result);
@@ -23,9 +44,14 @@ public class LensTest
     [TestMethod]
     public void TestSetLens()
     {
-        var teacher = new Teacher("John", 30);
+        var teacher = Teacher.New("John", 30);
         var lens = new language_ext_example.example.Lens();
+
+
         var result = lens.TeacherNameLens.Set("Tom", teacher);
+
+
+
         Assert.AreEqual("Tom", result.Name);
     }
 }
