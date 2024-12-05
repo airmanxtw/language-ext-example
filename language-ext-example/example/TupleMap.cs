6df64e8deb2592a5f4ff715af1653dc6161008e0
airmanxtw <airmanx@stust.edu.tw>
2024-12-05 23:58:51 +0800

    調整 TupleMap 類別中的 Add 方法，新增 Add_1 和 Add_2 方法以支持不同的元組加法實現

--- language-ext-example/example/TupleMap.cs
+++ language-ext-example/example/TupleMap.cs
@@ -4,6 +4,10 @@ namespace language_ext_example.example;
 public class TupleMap
 {
     public static int AddMethod(int x, int y) => x + y;
-    public static int Add(Tuple<int, int> t) => t.Map(AddMethod);
+    public static int Add_1(Tuple<int, int> t) => t.Map(AddMethod);
+
+    public static int Add_2(Tuple<int, int> t) => map(t, AddMethod);
+
+
 
 }
