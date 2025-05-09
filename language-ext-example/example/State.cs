2f421386a7eeddeeef9e1fbd49f35a723368916d
airmanxtw <airmanx@stust.edu.tw>
2025-05-09 14:17:15 +0800

    新增 State 類別以支持存款和取款操作，並新增對應的單元測試

---
+++ language-ext-example/example/State.cs
@@ -0,0 +1,10 @@
+namespace language_ext_example.example;
+using LanguageExt;
+using LanguageExt.Common;
+
+using static LanguageExt.Prelude;
+public class State
+{
+    public static State<int, int> DepositMoeney(int amount) => State<int, int>(s => (s, s + amount));
+    public static State<int, int> WithdrawMoney(int amount) => State<int, int>(s => (s, s - amount));
+}
---
+++ test-language-ext-example/Test/StateTest.cs
@@ -0,0 +1,15 @@
+using language_ext_example.example;
+using LanguageExt;
+using LanguageExt.UnitTesting;
+
+namespace test_language_ext_example.Test;
+[TestClass]
+public class StateTest
+{
+    [TestMethod]
+    public void TestState()
+    {
+        var money = State.DepositMoeney(500).Bind(_ => State.WithdrawMoney(200)).Run(100).State;
+        Assert.AreEqual(400, money);   
+    }
+}
